77f5b9e51511694d9a6d324dc6429c028c9915ee
Danny Lin <danny0838@gmail.com>
2014-08-08 23:36:12 +0800

    Basic enex conversion.

--- chrome/content/converter.js
+++ chrome/content/converter.js
@@ -61,20 +61,114 @@ function convert_enex2sb(input, output) {
     print("input directory: " + input.path);
     print("output directory: " + output.path);
     print("");
-
     var files = input.directoryEntries;
+    filesNext();
 
-    (function next() {
-        while(files.hasMoreElements()) {
+    function filesNext() {
+        while (files.hasMoreElements()) {
             var file = files.getNext().QueryInterface(Components.interfaces.nsIFile);
-            if (file.isFile()) {
-                print("converting '" + file.path + "'");
-                setTimeout(arguments.callee, 0);
-                return;
-            }
+            if (!file.isFile()) continue;
+            print("converting file: '" + file.path + "'");
+            parseEnex(loadXML(file));
+            return;
         }
         // finished
+        filesFinish();
+    }
+
+    function filesFinish() {
         print("");
         print("done.");
-    })();
+    }
+
+    function parseEnex(xmlDoc) {
+        var notesDOM = xmlDoc.getElementsByTagName("note");
+        var notes = [];
+        for (var i=0, I=notesDOM.length; i<I; i++) notes.push(notesDOM[i]);
+        notesNext();
+
+        function notesNext() {
+            var note = notes.shift();
+            if (note) {
+                parseNote(note);
+            }
+            else {
+                notesFinish();
+                return;
+            }
+            // next note (async)
+            setTimeout(notesNext, 0);
+        }
+
+        function notesFinish() {
+            // next file (async)
+            setTimeout(filesNext, 0);
+        }
+
+        function parseNote(note) {
+            var item = sbCommonUtils.newItem();
+
+            // basic meta data
+            item.type = "notex";
+            item.chars = "UTF-8";
+
+            // title
+            try {
+                item.title = note.getElementsByTagName("title")[0].textContent;
+            } catch(ex){}
+
+            // create
+            try {
+                item.create = parseEnexTime(note.getElementsByTagName("created")[0].textContent);
+            } catch(ex){}
+
+            // modify
+            try {
+                item.modify = parseEnexTime(note.getElementsByTagName("updated")[0].textContent);
+            } catch(ex){}
+
+            // source
+            try {
+                item.source = note.getElementsByTagName("source-url")[0].textContent;
+            } catch(ex){}
+            
+            // output
+            var destDir = getUniqueDir(output, item.title);
+            print("exporting note: '" + item.title + "' --> '" + destDir.leafName + "'");
+            var indexDat = destDir.clone(); indexDat.append("index.dat");
+            sbCommonUtils.writeIndexDat(item, indexDat);
+
+            function parseEnexTime(time) {
+                if (time.match(/^(\d{4})(\d{2})(\d{2})T(\d{2})(\d{2})(\d{2})Z$/)) {
+                    var date = new Date(
+                        parseInt(RegExp.$1, 10), parseInt(RegExp.$2, 10) - 1, parseInt(RegExp.$3, 10),
+                        parseInt(RegExp.$4, 10), parseInt(RegExp.$5, 10), parseInt(RegExp.$6, 10)
+                    );
+                    var date = new Date(date.valueOf() - date.getTimezoneOffset() * 60 * 1000);
+                    return sbCommonUtils.getTimeStamp(date);
+                }
+                return false;
+            }
+        }
+    }
+}
+
+function loadXML(file) {
+    var parser = new DOMParser();
+    return parser.parseFromString(sbCommonUtils.convertToUnicode(sbCommonUtils.readFile(file), "UTF-8"), "text/xml");
+}
+
+function getUniqueDir(dir, name) {
+    var name = sbCommonUtils.validateFileName(name).substring(0, 60) || "untitled";
+    name = name.replace(/\./g, "");
+    var num = 0, destDir, dirName;
+    do {
+        dirName = name;
+        if ( num > 0 ) dirName += "-" + num;
+        destDir = dir.clone();
+        destDir.append(dirName);
+    }
+    while ( destDir.exists() && ++num < 1024 );
+    destDir.create(destDir.DIRECTORY_TYPE, 0700);
+    return destDir;
 }
