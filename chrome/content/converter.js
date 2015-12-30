127866b183fd4b640fbb6f8c62b1a84611eed717
Danny Lin <danny0838@gmail.com>
2015-12-31 01:52:50 +0800

    Fix typo

--- chrome/content/converter.js
+++ chrome/content/converter.js
@@ -174,7 +174,7 @@ function convert_enex2sb(input, output, includeSubdir, includeFileName, uniqueId
                 var noteAttributesObj = note.getElementsByTagName("note-attributes")[0];
                 var sourceObj = noteAttributesObj.getElementsByTagName("source-url")[0];
                 var source = sourceObj.textContent;
-                // Evernote uses a blizzard url format for local files, fix it
+                // Evernote uses a bizard url format for local files, fix it
                 if (source.match(/^file:\/\/(.*)$/i)) {
                     source = sbConvCommon.convertFilePathToURL(RegExp.$1);
                 }
